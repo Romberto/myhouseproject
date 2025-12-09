481bc08102581126f8fbc58a61e8fb7672d57f6f
Romberto <korhoff@mail.ru>
2025-12-09 12:06:56 +0400

    refactor db

--- alembic/env.py
+++ alembic/env.py
@@ -11,6 +11,7 @@ from alembic import context
 
 from src.config import settings
 from src.core.models.base import Base
+from src.core.models.projects import Project, Image
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
